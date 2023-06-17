5a461908f63a0ca267ef5a96cd5973334ad5d440
Aswinsureshb <123205420+Aswinsureshb@users.noreply.github.com>
2023-06-17 00:51:34 -0400

    Update AdventureGame.java

--- AdventureGame.java
+++ AdventureGame.java
@@ -1,9 +1,12 @@
-import java.util.Scanner;
+Adventures of “Name of person playing”
+Class TextAdventure:
+It contains the rules for playing, the description of the game[Treasure hunt, here the player has to solve puzzles using their brain and should do as the puzzle tells them to for example, the key to a door is in a box and to find the box, we need to solve the puzzle written on the door. It will be like “Majestic and dangerous “ and the player has to select the most related object from the surroundings in which the correct selection unlocks the door and so on(can use if else statements)
+(Use println function for displaying the rules and description.)
+Press any key to continue…(can use a string and get input )
+Class AdventureModel:
+Get input from the player, execute them such as using string input such as show what all are there in the room. Then it tells the player about the surroundings, and the player chooses to move. Taking different objects along the path gives the player lives and moving to  different rooms gives points. Once a room is accessed, the player go there as many times as possible. Unlocking a room involves performing a task such as filling water or solving a puzzle. For solving puzzles correct answers should be given to questions asked. The answer to these questions are case sensitive and we can implement these by using if else statements.
+Class Adventurer:
+Contains the actions the character does such as take something, eat something, hit something, drop something etc. make an interface and add methods that corresponds to various actions.
+Class Rooms:
+Has a description of how it looks where everything is located(direction). Entering that room makes the code to display the description and the properties of various things in the room from which the player selects things according to their calculations.
 
-public class AdventureGame
-{
-    public static void main (String[]args)
-    {
-        
-    }
-}
